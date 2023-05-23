14a9f303d1348ac671691b45f365c05b3bd15134
Nguyen Tien Dung <forcewech@gmail.com>
2023-05-23 21:07:59 +0700

    sua giao dien

--- src/main/java/Database/DatabaseHelper.java
+++ src/main/java/Database/DatabaseHelper.java
@@ -1,32 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 /**
  *
- * @author thinh
+ * @author Administrator
  */
 public class DatabaseHelper {
-    public static final String connectionUrl = "jdbc:sqlserver://21AK22-COM\\SQLEXPRESS:1433;"+"databaseName=QLTaiSanPhongMay;user=sa;password=123456;encrypt=true;trustServerCertificate=true;";
+    public static final String connectionUrl = "jdbc:sqlserver://21AK22-COM\\SQLEXPRESS:1433" + 
+        "databaseName=QLTaiSanPhongMay;user=sa;password=123456;encrypt=true;trustServerCertificate=true;";
     public static Connection getDBConnection() {
-        Connection conn = null;
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        } catch(ClassNotFoundException ex) {
-            System.out.println("chua co Driver!" + ex.toString());
-        }
-        try {
-            conn = DriverManager.getConnection(connectionUrl);
-            return conn;
-        } catch(SQLException ex) {
-            System.out.println("Loi connection" + ex.toString());
-        }
-        return null;
+   Connection conn = null;
+    try {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    } catch(Exception ex) {
+        System.out.println("chua co Driver!" + ex.toString());
+    }
+    try {
+        conn = DriverManager.getConnection(connectionUrl);
+        return conn;
+    } catch(Exception ex) {
+        System.out.println("Loi connection" + ex.toString());
+    }
+    return null;
     }
 }
